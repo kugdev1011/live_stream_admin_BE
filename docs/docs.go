6e87d5234138a158f13981608bda7872e2f29580
kugdev1011 <kug76501@gmail.com>
2025-01-12 21:28:48 +0700

    remove dim line

--- docs/docs.go
+++ docs/docs.go
@@ -522,8 +522,6 @@ var SwaggerInfo = &swag.Spec{
 	Description:      "",
 	InfoInstanceName: "swagger",
 	SwaggerTemplate:  docTemplate,
-	LeftDelim:        "{{",
-	RightDelim:       "}}",
 }
 
 func init() {
