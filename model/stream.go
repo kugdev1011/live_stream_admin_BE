599e6403c4598119a6cea9bd8e876fcbfa62b33f
kugdev1011 <kug76501@gmail.com>
2025-01-15 14:28:37 +0700

    add NotificationType

--- model/stream.go
+++ model/stream.go
@@ -40,6 +40,14 @@ const (
 	ViewTypeRecordView ViewType = "record_view"
 )
 
+type NotificationType string
+
+const (
+	NotificationTypeSubscribeLive  NotificationType = "subscribe_live"
+	NotificationTypeSubscribeVideo NotificationType = "subscribe_video"
+	NotificationTypeBlocked        NotificationType = "account_blocked"
+)
+
 type Stream struct {
 	ID          uint         `gorm:"primaryKey"`
 	UserID      uint         `gorm:"not null"`
@@ -59,15 +67,15 @@ type Stream struct {
 }
 
 type Notification struct {
-	ID        uint         `gorm:"primaryKey"`
-	UserID    uint         `gorm:"column:user_id;not null"`
-	StreamID  uint         `gorm:"column:stream_id"`
-	Type      string       `gorm:"type:varchar(50);not null"`
-	Content   string       `gorm:"type:text;not null"`
-	CreatedAt time.Time    `gorm:"default:CURRENT_TIMESTAMP;not null"`
-	ReadAt    sql.NullTime `gorm:"column:read_at"`
-	Stream    Stream       `gorm:"foreignKey:StreamID;constraint:OnDelete:CASCADE"`
-	User      User         `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
+	ID        uint             `gorm:"primaryKey"`
+	UserID    uint             `gorm:"column:user_id;not null"`
+	StreamID  uint             `gorm:"column:stream_id"`
+	Type      NotificationType `gorm:"type:varchar(50);not null"`
+	Content   string           `gorm:"type:text;not null"`
+	CreatedAt time.Time        `gorm:"default:CURRENT_TIMESTAMP;not null"`
+	ReadAt    sql.NullTime     `gorm:"column:read_at"`
+	Stream    Stream           `gorm:"foreignKey:StreamID;constraint:OnDelete:CASCADE"`
+	User      User             `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 }
 
 // type Chat struct {
